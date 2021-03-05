e455687144e1b569a215cc5ec39d027ee62cc669
Anissa <anissajones10@gmail.com>
2021-03-05 02:29:22 -0500

    Fix Phonelib gem so it converts phone_number to correct format

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -11,26 +11,12 @@
 #  updated_at   :datetime         not null
 #
 class Conversation < ApplicationRecord
-  # before_save :normalize_phone
+  before_save :normalize_phone
 
   belongs_to :user
 
   validates :name, :text_message, presence: true
-  # validates :attribute, phone: true
-
-  # def formatted_phone
-  #   parsed_phone = Phonelib.parse(phone)
-  #   return phone if parsed_phone.invalid?
-
-  #   formatted =
-  #     if parsed_phone.country_code == "1" # NANP
-  #       parsed_phone.full_national # (415) 555-2671;123
-  #     else
-  #       parsed_phone.full_international # +44 20 7183 8750
-  #     end
-  #   formatted.gsub!(";", " x") # (415) 555-2671 x123
-  #   formatted
-  # end
+  validates :phone, phone: true
 
   private
 
